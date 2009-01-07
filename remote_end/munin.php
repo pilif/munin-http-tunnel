a9910a86d794955e2ae496c95a723b1958ff3399
Philip Hofstetter <phofstetter@sensational.ch>
2009-01-07 13:43:57 +0100

    cleanup

--- remote_end/munin.php
+++ remote_end/munin.php
@@ -3,11 +3,10 @@ $req = file_get_contents('php://input');
 $s = @fsockopen('localhost', 4949, $errno, $errstr, 10);
 if (!$s) { die("# connection error"); }
 $banner = fgets($s); // skip banner. Client outputs that
-$res = "";
 fwrite($s, $req."\n");
 
 if (!preg_match('#^(nodes|config|fetch)#', $req)){
-    $res .= fgets($s);
+    $res = fgets($s);
 }else{
     while(!feof($s)){
         $line = chop(fgets($s));
